d248f59185da22ed2f76ac20cf67469cdb685f79
César Higashi <76760288+higashi04@users.noreply.github.com>
2022-10-15 16:12:12 -0500

    project start

---
+++ src/images/images.js
@@ -0,0 +1,13 @@
+import dirtImg from './dirt.jpg';
+import glassImg from './glass.png';
+import grassImg from '.grass.png';
+import logImg from './log.jpg';
+import woodImg from './wood.png';
+
+export {
+    dirtImg,
+    glassImg,
+    grassImg,
+    logImg,
+    woodImg
+}
---
+++ src/images/textures.js
@@ -0,0 +1,24 @@
+import { /*NearestFilter,*/ TextureLoader, /*RepeatWrapping*/ } from three
+import {
+    dirtImg,
+    glassImg,
+    grassImg,
+    logImg,
+    woodImg
+} from './images'
+
+const dirtTexture = new TextureLoader().load(dirtImg);
+const glassTexture = new TextureLoader().load(glassImg);
+const grassTexture = new TextureLoader().load(grassImg);
+const logTexture = new TextureLoader().load(logImg);
+const woodTetxture = new TextureLoader().load(woodImg);
+const groundTexture = new TextureLoader().load(grassImg);
+
+export {
+    dirtTexture,
+    glassTexture,
+    grassTexture,
+    logTexture,
+    woodTetxture,
+    groundTexture
+}
